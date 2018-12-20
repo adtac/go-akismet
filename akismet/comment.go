dbf3f00afdaf3f6627d3305fc0e547ca9261ee14
Adhityaa Chandrasekar <adtac@adtac.in>
2018-12-19 22:22:21 -0500

    commento.go: go fmt

--- akismet/comment.go
+++ akismet/comment.go
@@ -11,8 +11,8 @@ type Comment struct {
 	CommentAuthorEmail string `form:"comment_author_email"`
 	CommentAuthorURL   string `form:"comment_author_url"`
 	CommentContent     string `form:"comment_content"`
+	BlogLang           string `form:"blog_lang"`
+	BlogCharset        string `form:"blog_charset"`
+	UserRole           string `form:"user_role"`
 	// TODO: Add support for comment_date_gmt and comment_post_modified_gmt
-	BlogLang    string `form:"blog_lang"`
-	BlogCharset string `form:"blog_charset"`
-	UserRole    string `form:"user_role"`
 }
